302491f1c1176d6847ce81752ebc92ffc34e274a
shin6585 <shinyujiro@gmail.com>
2016-03-09 12:25:41 +0000

    adjust render part

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'follwer_users/show'
+
+  get 'following_users/show'
+
   get 'users/edit'
 
   root to: 'static_pages#home'
@@ -7,8 +11,12 @@ Rails.application.routes.draw do
   post   'login' , to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
+
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts
   resources :relationships, only: [:create, :destroy]
+  resources :following_users, only: [:show]
+  resources :follower_users, only: [:show]
+
 end
